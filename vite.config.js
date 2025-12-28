d1ad7c88e02ef72cbf5910bed015890b3e341d48
Boris Diakur <borisdiakur@gmail.com>
2025-12-28 18:32:54 +0100

    🐶

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,13 @@ module.exports = defineConfig({
 			input: {
 				main: resolve(__dirname, 'index.html'),
 				en: resolve(__dirname, 'en/index.html'),
+				ja: resolve(__dirname, 'ja/index.html'),
 				imprint: resolve(__dirname, 'imprint/index.html'),
 				'en/imprint': resolve(__dirname, 'en/imprint/index.html'),
+				'ja/imprint': resolve(__dirname, 'ja/imprint/index.html'),
 				privacy: resolve(__dirname, 'privacy/index.html'),
 				'en/privacy': resolve(__dirname, 'en/privacy/index.html'),
+				'ja/privacy': resolve(__dirname, 'ja/privacy/index.html'),
 			},
 		},
 	},
